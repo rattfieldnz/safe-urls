9332995c007e1b8502df3f9cb48098b06944d063
Rob Attfield <emailme@robertattfield.com>
2019-08-04 01:12:44 +0000

    Apply fixes from StyleCI

--- tests/Libraries/Curl/CurlTest.php
+++ tests/Libraries/Curl/CurlTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace RattfieldNz\SafeUrls\Libraries\Curl;
 
-use Orchestra\Testbench\TestCase;;
+use Orchestra\Testbench\TestCase;
 
 class CurlTest extends TestCase
 {
